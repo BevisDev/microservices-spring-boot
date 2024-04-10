ad8894a8c1f0db5e5df66a49713cc3fbc331dec6
BevisDev <dev.binhtt@gmail.com>
2024-04-10 22:03:01 +0700

    demo send email outlook

---
+++ notification-service/src/main/java/com/notification/service/EmailService.java
@@ -0,0 +1,43 @@
+package com.notification.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendMail() {
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper;
+        try {
+            helper = new MimeMessageHelper(message, true, "UTF-8");
+            helper.setFrom("trthanhbinh5720@gmail.com");
+            String[] toEmails = new String[]{
+                    "dev.binhtt@gmail.com",
+                    "kimbapham96@gmail.com"
+            };
+
+            helper.setTo(toEmails);
+            helper.setSubject("[SUBJECT] EMAIL");
+            helper.setText("TESTST CONTENT", true);
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
